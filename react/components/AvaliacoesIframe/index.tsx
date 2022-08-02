da962799672c24314c1bc40bbc0c99c9be13a78f
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2022-08-01 21:12:18 -0300

    fix

--- react/components/AvaliacoesIframe/index.tsx
+++ react/components/AvaliacoesIframe/index.tsx
@@ -9,7 +9,7 @@ const AvaliacoesIframe: StorefrontFunctionComponent<ProductAvailableProps> = () 
 
         return (
          <>
- <iframe id='AV_widget_iframe' frameBorder="0" width="100%" height="100%" src="//cl.avis-verifies.com/br/cache/d/7/2/d726a728-f205-6ae4-7dd0-31bb2fe6e2c6/widget4/d726a728-f205-6ae4-7dd0-31bb2fe6e2c6horizontal_index.html"></iframe>
+ <iframe id='AV_widget_iframe' frameBorder="0" width="100%" height="315px" src="//cl.avis-verifies.com/br/cache/d/7/2/d726a728-f205-6ae4-7dd0-31bb2fe6e2c6/widget4/d726a728-f205-6ae4-7dd0-31bb2fe6e2c6horizontal_index.html"></iframe>
          </>
         );
     };
