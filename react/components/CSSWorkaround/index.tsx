eb31cdcd65b9f4e0aed659f4e7a441d2ac25756a
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2022-08-18 23:21:11 -0300

    fixes

---
+++ react/components/CSSWorkaround/index.tsx
@@ -0,0 +1,17 @@
+import React   from "react";
+import "./global.css";
+interface ProductAvailableProps {
+    // children: any;
+};
+
+const CSSWorkaround: StorefrontFunctionComponent<ProductAvailableProps> = () => {
+    
+
+        return (
+         <>
+   
+         </>
+        );
+    };
+
+export default CSSWorkaround;
